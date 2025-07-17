46427fa6b14c5ebe14311efab784561c171f8b33
NathanMahpood <nathantest360@gmail.com>
2025-07-17 11:03:37 +0300

    added roles to user model

--- models/user.model.js
+++ models/user.model.js
@@ -16,7 +16,8 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    default: "user",
+    enum: ["passenger", "driver", "admin"],
+    default: "passenger",
   },
 });
 
